22126fe5d1537ff59be42da6f64f64b5d9e9f460
chinmaya panigrahy <cpanigrahy869@gmail.com>
2026-02-03 06:48:00 +0530

    db path change

--- server/config/db.js
+++ server/config/db.js
@@ -5,4 +5,4 @@ const connectDB = async ()=>{
     mongoose.connection.on('connected',() =>console.log('Database connnected'))
     await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
 }
-export default connectDB
+export default connectDB;
